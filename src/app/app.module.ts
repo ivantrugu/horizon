bed4a03119e57287ec09adc449573912f9ff5664
ivantrugu <ivantrugu@gmail.com>
2022-06-27 12:22:58 -0500

    primera adición proyecto horizon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    BannerComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
