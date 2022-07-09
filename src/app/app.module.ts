b413e08e0f2b1aace53f6620fe2892e8733b8080
saraimenco <saraimencod@gmail.com>
2022-07-09 17:45:56 -0500

    Estructura inicial frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
@@ -8,6 +9,12 @@ import { FooterComponent } from './component/footer/footer.component';
 import { BannerComponent } from './component/banner/banner.component';
 import { ContactComponent } from './component/contact/contact.component';
 import { LoginComponent } from './component/login/login.component';
+import { CarruselComponent } from './component/carrusel/carrusel.component';
+import { NormatividadComponent } from './component/normatividad/normatividad.component';
+import { NosotrosComponent } from './component/nosotros/nosotros.component';
+import { ServiciosComponent } from './component/servicios/servicios.component';
+
+
 
 @NgModule({
   declarations: [
@@ -16,11 +23,17 @@ import { LoginComponent } from './component/login/login.component';
     FooterComponent,
     BannerComponent,
     ContactComponent,
-    LoginComponent
+    CarruselComponent,
+    NormatividadComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
